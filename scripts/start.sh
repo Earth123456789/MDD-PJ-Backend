7a4ca4ee5afcaddf3e4eb8bb10c64a293633d34f
Pisol Uattankanjana <pisol.kanomjeen.uatt@gmail.com>
2025-03-07 21:02:53 +0700

    update driver service

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Building all services..."
-
-docker-compose -f ./docker/docker-compose.yml build
-
-echo "Starting the Docker containers..."
-
-docker-compose -f ./docker/docker-compose.yml up
